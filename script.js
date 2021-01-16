248d039b35d6e392c528959da42e5075ef6fcd7e
Justin Westmoreland <justinrwestmoreland@gmail.com>
2021-01-16 13:26:20 -0700

    initial commit sorry about the readme

---
+++ script.js
@@ -0,0 +1,23 @@
+// Click a button to start a game
+
+// Display number of blanks from random word that user is trying to guess
+
+// Need to time the game 
+
+// User wins the game when all letters have been correctly guessed
+
+// User losed the game when time runs out before all letters are guessed
+
+// Display total wins and losses 
+
+
+// When user selects a key, log that as a key event
+
+// When correct letter is guessed, _ should be replaced with that letter
+
+// Program an alert when the game is finished with a win or lose message
+
+// when start button is clicked, timer should restart
+
+// Wins and losses should persist, so make sure they are in local storage
+
